26c0d78ef401902be67e5ae84f1e28699ba5fc9d
alexlastracr7 <lastra@sfedu.ru>
2022-07-17 18:31:03 +0300

    case de datos enviado

---
+++ app/code/Shellpea/NewModule/Controller/Page/Save.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Shellpea\NewModule\Controller\Page;
+
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class View implements HttpGetActionInterface
+{
+
+    protected $_pageFactory;
+    /**
+     * @var \Magento\Framework\App\RequestInterface
+     */
+    private $request;
+    /**
+     * @var \Shellpea\NewModule\Model\PostFactory
+     */
+    private $postFactory;
+
+    public function __construct(
+        \Magento\Framework\View\Result\PageFactory $pageFactory,
+        \Shellpea\NewModule\Model\PostFactory $postFactory,
+        \Magento\Framework\App\RequestInterface $request
+    ) {
+        $this->_pageFactory = $pageFactory;
+        $this->postFactory = $postFactory;
+        $this->request = $request;
+    }
+
+    public function execute()
+    {
+        var_dump($this->request->getParams());
+        return $this->_pageFactory->create();
+    }
+
+}
